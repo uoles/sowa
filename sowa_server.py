04af7b29bf54b11431286785d930e6c5650bc8bf
uoles <max.uoles@rambler.ru>
2024-02-15 05:40:14 +0300

    Add server for sowa.

--- sowa_server.py
+++ sowa_server.py
@@ -1,12 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 from flask_restful import Api
 
 import os.path
 import logging
 
-logging.basicConfig()
-log = logging.getLogger()
+
+handler = logging.StreamHandler()
+handler.setFormatter(logging.Formatter('%(asctime)s %(name)-12s %(levelname)-8s %(message)s'))
+
+log = logging.getLogger('sowa_server')
 log.setLevel(logging.INFO)
+log.addHandler(handler)
+
+log_flask = logging.getLogger('werkzeug')
+log_flask.disabled = True
 
 
 def root_dir():
@@ -22,6 +29,12 @@ def get_file(filename):
         return str(exc)
 
 
+def save_file(filename, content):
+    log.info("Сохраняем изменения в файл '{}':\n{}".format(filename, content))
+    with open(filename, 'w', encoding='utf-8', newline='') as f:
+        f.write(content)
+
+
 app = Flask(__name__)
 api = Api(app)
 
@@ -33,29 +46,8 @@ def home():
 
 @app.route('/save_bad_words', methods=['POST'])
 def save_bad_words():
-    return render_template('index.html')
-
-# @app.route('/', methods=['GET'])
-# def home():
-#     content = get_file('templates/index.html')
-#     return Response(content, mimetype="text/html")
-
-
-# @app.route('/resources/templates/css/bootstrap.min.css', methods=['GET'])
-# @app.route('/resources/templates/js/jquery.min.js', methods=['GET'])
-# @app.route('/resources/templates/js/bootstrap.min.js', methods=['GET'])
-# @app.route('/resources/bad_words.txt', methods=['GET'])
-# def resources_txt():
-#     print('---- Resources path - ' + str(request.path))
-#     content = get_file(request.path[1:].replace("/", "\\"))
-#     return Response(content, mimetype="text/html")
-#
-#
-# @app.route('/resources/templates/image/sbIch.jpg', methods=['GET'])
-# def resources_image():
-#     print('---- Resources path - ' + str(request.path))
-#     content = get_file(request.path[1:].replace("/", "\\"))
-#     return Response(content, mimetype="image/jpeg", content_type="image/jpeg")
+    save_file('./static/bad_words.txt', request.form['badWords'])
+    return redirect('/')
 
 
 if __name__ == '__main__':
